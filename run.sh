ee8cd98c76a035a2d004e69b69c126b2f45db810
Ubuntu <igor@devel.bu.f3.internal.cloudapp.net>
2015-11-03 10:42:25 +0000

    Changed ports for ssh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo docker run -d -p 127.0.0.1:224:22 ivs0/b-arm-m
+sudo docker run -d -p 127.0.0.1:8224:22 ivs0/b-arm-m
 
-sudo docker run -d -p 127.0.0.1:225:22 ivs0/b-clang
+sudo docker run -d -p 127.0.0.1:8225:22 ivs0/b-clang
